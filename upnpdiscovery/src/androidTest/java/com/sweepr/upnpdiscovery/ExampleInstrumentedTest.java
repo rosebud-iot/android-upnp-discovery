9bf9f36273b31d984bda60159075e16afe717712
Paweł Hofman <pawel.hofman@sweepr.com>
2023-04-21 21:12:23 +0200

    Added some basic discovery test

--- upnpdiscovery/src/androidTest/java/com/sweepr/upnpdiscovery/ExampleInstrumentedTest.java
+++ upnpdiscovery/src/androidTest/java/com/sweepr/upnpdiscovery/ExampleInstrumentedTest.java
@@ -1,14 +1,20 @@
 package com.sweepr.upnpdiscovery;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import androidx.annotation.NonNull;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -17,11 +23,41 @@ import androidx.test.runner.AndroidJUnit4;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private static final String TAG = "ExampleInstrumentedTest";
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        final Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.octarine.upnpdiscovery.test", appContext.getPackageName());
+        assertEquals("com.sweepr.upnpdiscovery.androidTest", appContext.getPackageName());
+    }
+
+    @Test
+    public void run_discovery_for_10sec() {
+        final Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        UPnPDiscovery.discoveryDevices(appContext, executor, 10000, null, new UPnPDiscovery.OnDiscoveryListener() {
+            @Override
+            public void onDiscoveryStart() {
+                Log.d(TAG, "Discovery started");
+            }
+
+            @Override
+            public void onDiscoveryFoundNewDevice(@NonNull UPnPDevice device) {
+                Log.d(TAG, "Device found: " + device.getFriendlyName());
+            }
+
+            @Override
+            public void onDiscoveryFinish(@NonNull Set<UPnPDevice> devices) {
+                Log.d(TAG, "Discovery finished with: " + devices.size() +  " devices");
+            }
+
+            @Override
+            public void onDiscoveryError(@NonNull Exception e) {
+                Log.e(TAG, "Error: " + e.getLocalizedMessage());
+            }
+        });
     }
 }
